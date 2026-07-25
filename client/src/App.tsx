3e512ff66b2c2c477c2188f8e386ba15f5c1d072
albertoramos98 <albertinhorss@gmail.com>
2026-07-25 17:40:49 -0300

    chore: remove spotify radio player module

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,6 @@ import NotFound from "@/pages/NotFound";
 import { Route, Switch, useLocation } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
-import { RadioPlayer } from "./components/RadioPlayer";
 import Home from "./pages/Home";
 import Admin from "./pages/Admin";
 import HowItWorks from "./pages/HowItWorks";
@@ -48,7 +47,6 @@ function App() {
         <TooltipProvider>
           <Toaster />
           <Router />
-          <RadioPlayer />
         </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
